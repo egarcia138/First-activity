a4fdb7677a311e997a2288dab7f95fe26ee3a43e
Emilio Garcia <EGarcia138@my.gcu.edu>
2020-01-08 15:02:39 -0700

    First Commit

---
+++ InClassActivity/Source.cpp
@@ -0,0 +1,14 @@
+//Emilio Garcia
+//This is my own work
+//In class activity 1/8/2020
+#include<iostream>
+using namespace std;
+int main()
+{
+    float kilogram, pound;
+    cout << "Enter pound ";
+    cin >> pound;
+    kilogram = pound * .454;
+    cout << pound << " pound = " << kilogram << " kg";
+    return 0;
+}
