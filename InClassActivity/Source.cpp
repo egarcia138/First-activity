51188ad28d952fa565753f0998fe91e00a812a50
Emilio Garcia <EGarcia138@my.gcu.edu>
2020-01-10 14:51:39 -0700

    Finished activity 2 & 3

--- InClassActivity/Source.cpp
+++ InClassActivity/Source.cpp
@@ -12,3 +12,42 @@ int main()
     cout << pound << " pound = " << kilogram << " kg";
     return 0;
 }
+
+//Emilio Garcia
+//This is my own work
+//In class activity 1/8/2020
+#include<iostream>
+using namespace std;
+int main()
+{
+    double side, area;
+    cout << "enter the side of hexagon" << endl ;
+    cin >> side;
+    area = (3 * 1.732 * side * side) / 2;
+    cout << "area of hexagon is: " << area << endl ;
+    system("pause");
+    return 0;
+}
+
+//Emilio Garcia
+//This is my own work
+//In class activity 1/8/2020
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Enter first number:";
+    cin >> num1;
+    cout << "Enter second number:";
+    cin >> num2;
+    if (num1 > num2)
+    {
+        cout << "First number " << num1 << " is the largest";
+    }
+    else
+    {
+        cout << "Second number " << num2 << " is the largest";
+    }
+    return 0;
---
+++ enc_temp_folder/e24cacacfe5a33e058bdad31cb090f6/Source.cpp
@@ -0,0 +1,31 @@
+//Emilio Garcia
+//This is my own work
+//In class activity 1/8/2020
+#include<iostream>
+using namespace std;
+int main()
+{
+    float kilogram, pound;
+    cout << "Enter pound ";
+    cin >> pound;
+    kilogram = pound * .454;
+    cout << pound << " pound = " << kilogram << " kg";
+    return 0;
+}
+
+//Emilio Garcia
+//This is my own work
+//In class activity 1/8/2020
+#include<iostream>
+using namespace std;
+int main()
+{
+    double side, area;
+    cout << "enter the side of hexagon" << endl;
+    cin >> side;
+    area = (3 * 1.732 * side * side) / 2;
+    cout << "area of hexagon is: " << area << endl;
+    system("pause");
+    return 0;
+}
+
